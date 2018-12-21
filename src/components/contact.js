f95ab907d657e760e34567fd140510d567c48504
Greg McKelvey <mckelveygreg@gmail.com>
2018-12-21 14:48:52 -0800

    add fontawesome

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import Separator from './separator';
-import styled from 'styled-components' 
+import Separator from './separator'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faTwitter,
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons'
 
 const ContactContainer = styled.div`
   display: grid;
@@ -9,19 +15,46 @@ const ContactContainer = styled.div`
   grid-template-columns: repeat(3, auto);
   grid-gap: 10px;
 `
+const A = styled.a`
+   text-decoration: none;
+  color: black;
+  margin: 10px;
+
+  & svg {
+    font-size: 3rem;
+  }
+`
 // TODO: install fa-icons
 const SocMeds = () => (
-  <div>
-    Facebook
-    LinkedIn
-    Twitter
+  <div className="socMeds">
+    <A
+      href="https://www.facebook.com/trifoia"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faFacebook} />
+    </A>
+    <A
+      href="https://www.linkedin.com/company/trifoia/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faLinkedin} />
+    </A>
+    <A
+      href="https://twitter.com/TeamTrifoia"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faTwitter} />
+    </A>
   </div>
 )
 
 const Contact = () => {
   return (
     <ContactContainer>
-      <a href="https://trifoia.com/say-hello" >Say Hello</a>
+      <a href="https://trifoia.com/say-hello">Say Hello</a>
       <Separator />
       <SocMeds />
     </ContactContainer>
